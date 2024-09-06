f1578ec9a5346042ecf40bf19df511d27dc1d997
dzakia-st3 <annisadzakiaramadhina22@gmail.com>
2024-09-06 21:36:27 +0700

    Reuseable Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
+import star from './img/star.png'
+import fire from './img/fire.svg'
+import home from './img/home.svg'
+import left from './img/left.svg'
+import right from './img/right.svg'
 import './App.css';
 import Buttonhouter from './component/button';
 
@@ -7,7 +12,72 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Buttonhouter text='Test Button' disabled={false}/>
+        <Buttonhouter
+          img={star}
+          text='Test Button'
+          disabled={false}
+          iconPosition='right'
+          textColor='text-white-600'
+          bakcgroundColor=''
+          hovertrue= {true}
+          hoverimg='null'
+          hovercolor= 'bg-orange-200'
+        />
+        <Buttonhouter
+          img={fire}
+          text='Popular'
+          disabled={false}
+          iconPosition='left'
+          textColor='text-red-600'
+          bakcgroundColor='bg-red-100'
+          hovertrue= {false}
+          hoverimg='null'
+          hovercolor= ''
+        />
+        <Buttonhouter
+          img={home}
+          text='Home'
+          disabled={false}
+          iconPosition='left'
+          textColor='text-white-600'
+          bakcgroundColor=''
+          hovertrue= {true}
+          hoverimg='null'
+          hovercolor= 'bg-orange-200'
+        />
+        <Buttonhouter
+          img={left}
+          text='null'
+          disabled={false}
+          iconPosition=''
+          textColor='text-white-600'
+          bakcgroundColor=''
+          hovertrue= {true}
+          hoverimg='null'
+          hovercolor= 'bg-orange-200'
+        />
+        <Buttonhouter
+          img={right}
+          text='Search'
+          disabled={false}
+          iconPosition='right'
+          textColor='text-white-600'
+          bakcgroundColor='bg-emerald-500'
+          hovertrue= {true}
+          hoverimg='null'
+          hovercolor= 'bg-orange-200'
+        />
+        <Buttonhouter
+          img='null'
+          text='Without Icon'
+          disabled={false}
+          iconPosition=''
+          textColor='text-white-600'
+          bakcgroundColor=''
+          hovertrue= {true}
+          hoverimg='null'
+          hovercolor= 'bg-orange-200'
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,21 +1,45 @@
 import React from "react";
-import star from '../img/star.png'
 
 interface ButtonProps {
+    img: string;
     text: string;
     disabled: boolean;
-    // isleft: boolean;
+    iconPosition: string;
+    textColor: string;
+    bakcgroundColor: string;
+    hovertrue: boolean;
+    hoverimg: string;
+    hovercolor: string;
     // onclick: (() => {})
-  }
+}
 
 
-function Buttonhouter({ text, disabled }: Readonly<ButtonProps>) {    
+function Buttonhouter({ img, text, disabled, iconPosition, textColor, bakcgroundColor, hovertrue, hoverimg, hovercolor }: Readonly<ButtonProps>) {
+    let ComponentInside;
+
+    if (img === 'null') {
+        ComponentInside = <button className="mx-1" disabled={disabled}>{text}</button>
+    } else if (text === 'null') {
+        ComponentInside = <img className = 'size-4 mx-1' src={img} alt='icon' />
+    } else if (iconPosition.toLowerCase() === 'right') {
+        ComponentInside = 
+            <>
+                <button className="mx-1" disabled={disabled}>{text}</button>
+                <img className = 'size-4 mx-1' src={img} alt='icon' />
+            </>
+    } else if (iconPosition.toLowerCase() === 'left') {
+        ComponentInside = 
+        <>
+            <img className = 'size-4 mx-1' src={img} alt='icon' />
+            <button className="mx-1" disabled={disabled}>{text}</button>
+        </>
+    }
+
+    let styling = `flex justify-between text-sm border-2 border-${textColor}-400 ${bakcgroundColor === '' ? '' : bakcgroundColor} ${textColor} p-2 rounded-full mb-2 hover:bg-red-100`
+
     return (
-      <div className="flex justify-between text-sm border-gray-400 bg-slate-50 text-gray-600 p-2 rounded-full">
-      <img className = 'size-4 mx-1' src={star} alt='icon' />
-      <button className="mx-1" disabled={disabled}>{text}</button>
-      </div>
-    );
-  }
+        <div className={styling}>{ComponentInside}</div>
+    )
+}
 
 export default Buttonhouter
