afa348fbe091a0e4889364ae95bec6976d07d087
dzakia-st3 <annisadzakiaramadhina22@gmail.com>
2024-09-13 21:02:17 +0700

    Section 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import fire from './img/fire.svg'
 import home from './img/home.svg'
 import left from './img/left.svg'
 import right from './img/right.svg'
+import imghero from './img/img_hero.svg'
 import './App.css';
 import { Buttonhouter, TextHouter } from './component/smallcomponent'
 
@@ -12,8 +13,27 @@ function App() {
   return (
     <div>
       <header className="header">
-          <div></div>
-          <div></div>
+        <div className='section-1 flex justify-between'>
+          <div>
+            <TextHouter
+              text="Find The Place To"
+              fontstyle="font-bold font-lexend text-4xl text-[#1b1c57]"
+            />
+            <div className="flex">
+              <TextHouter
+                text="Live"
+                fontstyle='font-bold font-lexend text-4xl text-[#1b1c57]'
+              />
+              <TextHouter
+                text="Your Dreams"
+                fontstyle='font-bold font-lexend text-4xl text-[#1b1c57] underline mx-3'
+              />
+            </div>
+          </div>
+          <div>
+            <img src={imghero} alt="" />
+          </div>
+        </div>
       </header>
     </div>
   );
