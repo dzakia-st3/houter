a3e06bdaa41e3f8dfbcc2d1e54eb38df515dc06f
dzakia-st3 <annisadzakiaramadhina22@gmail.com>
2024-09-23 14:55:21 +0700

    Section 3

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,19 @@ import line from './img/line.svg'
 import homeblue from './img/home-blue.svg'
 import fire from './img/fire.svg'
 import wallet from './img/wallet-green.svg'
+import bed from './img/bed 1.svg'
+import garage from './img/car-garage 1.svg'
+import bath from './img/bath 1.svg'
+import stairs from './img/stairs-with-handrail 1.svg'
+import phone from './img/phone.svg'
+import tableimg from './img/table.svg'
+import sofa from './img/sofa.svg'
+import chair from './img/chair.svg'
+import playbtn from './img/play btn.svg'
 import './App.css';
-import { Buttonhouter, TextHouter, MiniCard } from './component/smallcomponent'
+import { Buttonhouter, TextHouter, MiniCard, MiniLGCard, MiniSMCard } from './component/smallcomponent'
 import { Navbar } from './component/NavbarFooter';
+import { table } from 'console';
 
 function App() {
     let dataimage1 = [
@@ -308,7 +318,7 @@ function App() {
                 </div>
             </div>
             <div className='section-2'>
-                <div className='flex items-center justify-around w-full space-y-16'>
+                <div className='flex items-center justify-around w-full my-16'>
                     <div>
                         <div className='flex space-x-2'>
                             <img src={line} alt="" />
@@ -992,6 +1002,88 @@ function App() {
                                 <p className="font-lexend text-xs text-gray-400">Preston Rd. Inglewood, Maine 98380</p>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className='section-3 flex items-center justify-around py-12'>
+                <div className='w-1/3 p-10'>
+                    <div className='flex space-x-2'>
+                        <img src={line} alt="" />
+                        <TextHouter
+                            text='Ready To Sell'
+                            fontstyle="font-lexend text-base text-[#F59E0B]"
+                        />
+                    </div>
+                    <TextHouter
+                        text="Let's Tour And See Our House!"
+                        fontstyle="mt-2 font-bold font-lexend text-xl text-[#1b1c57]"
+                    />
+                    <TextHouter
+                        text="Houses recommended by our partners that have been curated to become the home of your dreams!"
+                        fontstyle="font-lexend text-base mt-4 text-gray-400"
+                    />
+                    <div>
+                        <TextHouter
+                            text="House Detail"
+                            fontstyle="font-lexend font-bold text-base mt-4 text-gray-600"
+                        />
+                        <div className="flex justify-between">
+                            <div>
+                                <div>
+                                    <MiniLGCard
+                                        img={bed}
+                                        text="4 Bedrooms"
+                                    />
+                                </div>
+                                <div>
+                                    <MiniLGCard
+                                        img={garage}
+                                        text="1 Carport"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <MiniLGCard
+                                        img={bath}
+                                        text="2 Bathrooms"
+                                    />
+                                </div>
+                                <div>
+                                    <MiniLGCard
+                                        img={stairs}
+                                        text="2 Floors"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="flex items-center justify-between">
+                        <MiniSMCard
+                            img={profil1}
+                            name="Dianne Russell"
+                            info="Manager Director"
+                        />
+                        <Buttonhouter
+                            img={phone}
+                            text="Contact Now"
+                            disabled={true}
+                            iconPosition="left"
+                            textColor="text-white"
+                            bakcgroundColor='bg-[#10B981]'
+                            hovercolor='hover:bg-emerald-400'
+                        />
+                    </div>
+                </div>
+                <div className='relative'>
+                    <div className="w-96 h-96 rounded-xl bg-[url('./img/section3_house.svg')] flex justify-center bg-cover items-center p-2">
+                        <img src={playbtn} alt="" />
+                    </div>
+                    <div className='absolute -bottom-8 -left-36 flex space-x-2 items-end'>
+                        <img src={sofa} className='h-40 w-auto' alt="" />
+                        <img src={tableimg} className='h-20 w-auto' alt="" />
+                        <img src={chair} className='h-20 w-auto' alt="" />
                     </div>
                 </div>
             </div>
--- src/component/smallcomponent.tsx
+++ src/component/smallcomponent.tsx
@@ -20,7 +20,18 @@ interface MiniCardProps {
     listimageandstyle: Array<Object>;
     texttop: string;
     textbottom: string;
-} 
+}
+
+interface MiniLGCardProps {
+    img: string;
+    text: string;
+}
+
+interface MiniSMCardProps {
+    img: string;
+    name: string;
+    info: string;
+}
 
 function Buttonhouter({ img, text, disabled, iconPosition, textColor, bakcgroundColor, hovercolor }: Readonly<ButtonProps>) {
     let ComponentInside;
@@ -28,19 +39,19 @@ function Buttonhouter({ img, text, disabled, iconPosition, textColor, bakcground
     if (img === 'null') {
         ComponentInside = <button className="mx-1 font-lexend" disabled={disabled}>{text}</button>
     } else if (text === 'null') {
-        ComponentInside = <img className = 'size-4 mx-1' src={img} alt='icon' />
+        ComponentInside = <img className='size-4 mx-1' src={img} alt='icon' />
     } else if (iconPosition.toLowerCase() === 'right') {
-        ComponentInside = 
+        ComponentInside =
             <>
                 <button className="mx-1 font-lexend" disabled={disabled}>{text}</button>
-                <img className = 'size-4 mx-1' src={img} alt='icon' />
+                <img className='size-4 mx-1' src={img} alt='icon' />
             </>
     } else if (iconPosition.toLowerCase() === 'left') {
-        ComponentInside = 
-        <>
-            <img className = 'size-4 mx-1' src={img} alt='icon' />
-            <button className="font-lexend" disabled={disabled}>{text}</button>
-        </>
+        ComponentInside =
+            <>
+                <img className='size-4 mx-1' src={img} alt='icon' />
+                <button className="font-lexend" disabled={disabled}>{text}</button>
+            </>
     }
 
     let styling = `flex justify-between text-sm border-2 border-gray-200 ${bakcgroundColor === '' ? '' : bakcgroundColor} ${textColor} p-2 rounded-full ${hovercolor}`
@@ -50,19 +61,19 @@ function Buttonhouter({ img, text, disabled, iconPosition, textColor, bakcground
     )
 }
 
-function TextHouter ({ text, fontstyle }: Readonly<TextProps>) {
+function TextHouter({ text, fontstyle }: Readonly<TextProps>) {
     return (
         <p className={`${fontstyle}`}>{text}</p>
     )
 }
 
-function MiniCard ({listimageandstyle, texttop, textbottom} : Readonly<MiniCardProps> ) {
+function MiniCard({ listimageandstyle, texttop, textbottom }: Readonly<MiniCardProps>) {
     return (
         <div className="flex-none">
             <div className="flex items-center justify-center bg-white rounded-3xl p-4 h-24">
                 <div className='flex mr-2'>
                     {
-                        listimageandstyle.map( (item: any) => <img src={`${item.img}`} className={item.style} alt="" />)
+                        listimageandstyle.map((item: any) => <img src={`${item.img}`} className={item.style} alt="" />)
                     }
                 </div>
                 <div className=''>
@@ -74,4 +85,30 @@ function MiniCard ({listimageandstyle, texttop, textbottom} : Readonly<MiniCardP
     )
 }
 
-export {Buttonhouter, TextHouter, MiniCard}
+function MiniLGCard({ img, text }: Readonly<MiniLGCardProps>) {
+    return (
+        <div className='flex items-center p-2'>
+            <img src={img} className='mr-2' alt="" />
+            <TextHouter
+                text={text}
+                fontstyle="font-lexend font-bold text-base text-gray-600"
+            />
+        </div>
+    )
+}
+
+function MiniSMCard({img, name, info} : Readonly<MiniSMCardProps>) {
+    return (
+        <div className="flex space-x-2 items-center p-2">
+            <div className='flex'>
+                <img src={img} className='w-14 h-14' alt="" />
+            </div>
+            <div>
+                <p className="font-bold font-lexend text-base text-[#1b1c57]">{name}</p>
+                <p className="font-lexend text-base text-gray-400">{info}</p>
+            </div>
+        </div>
+    )
+}
+
+export { Buttonhouter, TextHouter, MiniCard, MiniLGCard, MiniSMCard}
